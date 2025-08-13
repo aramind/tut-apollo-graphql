4675e2f08824ef16952652d7c8c3e57db4f6c9bf
aramind <88814622+aramind@users.noreply.github.com>
2025-08-13 23:06:11 +0800

    feat: setting up the server with resolvers and typedefs

---
+++ server/mockDB.js
@@ -0,0 +1,5 @@
+export const users = [
+  { id: "1", name: "John Doe", age: 30, isMarried: true },
+  { id: "2", name: "Jane Smith", age: 25, isMarried: false },
+  { id: "3", name: "Alice Johnson", age: 28, isMarried: false },
+];
